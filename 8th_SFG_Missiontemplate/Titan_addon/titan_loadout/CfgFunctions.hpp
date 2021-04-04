435257bf747aaaa1e765d8f5332440836f7a7a44
Fallingstorm <fallingstorm@freenet.de>
2021-04-04 10:04:58 +0200

    CfgConfig added lrr and lrr_muni

--- 8th_SFG_Missiontemplate/Titan_addon/titan_loadout/CfgFunctions.hpp
+++ 8th_SFG_Missiontemplate/Titan_addon/titan_loadout/CfgFunctions.hpp
@@ -9,6 +9,8 @@ class titan
         class addmedicalequipment {};
         class atmo {};
         class atmo_muni {};
+        class lrr {};
+        class lrr_muni {};
         class removeitems {};
         class loadout_rearm {};
         class startloadout {};
