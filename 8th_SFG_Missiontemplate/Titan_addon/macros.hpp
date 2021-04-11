a4722da3cb9fbcc15b8fbfe4d417057f71b4fdfa
Fallingstorm <fallingstorm@freenet.de>
2021-04-11 12:16:27 +0200

    Path error fixed for sts

--- 8th_SFG_Missiontemplate/Titan_addon/macros.hpp
+++ 8th_SFG_Missiontemplate/Titan_addon/macros.hpp
@@ -1,4 +1,4 @@
-#define PREFIX TitanAddons
+#define PREFIX Titan_addon
 #include "\x\cba\addons\main\script_macros_mission.hpp"
 
 #define INIT_MODULE(var1) call compileFinal preprocessFileLineNumbers QUOTE(PREFIX\var1\init.sqf)
