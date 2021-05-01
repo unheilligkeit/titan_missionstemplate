a33bfe2d4be26e6c9f6fa69ff5f4206f43c05541
unheilligkeit <tartarch@hotmail.com>
2021-05-01 18:59:59 +0200

    add mission function

--- 8th_SFG_Missiontemplate/Titan_addon/titan_missionsbau/CfgFunctions.hpp
+++ 8th_SFG_Missiontemplate/Titan_addon/titan_missionsbau/CfgFunctions.hpp
@@ -6,7 +6,14 @@ class titan
     class titan_spawn_functions
     {
         file = "Titan_addon\titan_missionsbau\spawn_functions";
-        
+        class farming {};
+        class hunting {};
+    };
+
+    class titan_client_functions
+    {
+        file = "Titan_addon\titan_missionsbau\client";
+        class teleport_to_group {};
     };
 
 };
