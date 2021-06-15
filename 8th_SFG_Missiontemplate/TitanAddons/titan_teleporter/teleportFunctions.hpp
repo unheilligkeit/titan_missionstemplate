3664e186d4502c0e8192ba067e9aa3e7ff1aeda7
unheilligkeit <tartarch@hotmail.com>
2021-06-15 18:53:50 +0200

    add teleporter

---
+++ 8th_SFG_Missiontemplate/TitanAddons/titan_teleporter/teleportFunctions.hpp
@@ -0,0 +1,14 @@
+class titanTP
+{
+
+    tag = "titanTP";
+
+    class titan_spawn_functions
+    {
+        file = "TitanAddons\titan_teleporter\functions";
+        class teleport_to_group {};
+        class ace_teleport {};
+
+    };
+
+};
